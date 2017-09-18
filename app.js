8bb6869bdcf8c573156e5fe11dbf3f4bc88a2d62
susansico <susan.sico@gmail.com>
2017-09-17 18:27:34 -0700

    Added custom resize directive to app.js and index.html to research at what widths the navigation bar links change position as the screen width becomes smaller.

--- app.js
+++ app.js
@@ -24,3 +24,21 @@ app.controller("NavbarCollapse",["$scope", function($scope) {
 
     $scope.isNavCollapsed = true;
 }]);
+
+app.directive("resize", function($window) {
+
+    return function(scope) {
+
+        scope.width = $window.innerWidth;
+        scope.height = $window.innerHeight;
+        angular.element($window).on("resize", function() {
+
+            scope.$apply(function() {
+
+                scope.width = $window.innerWidth;
+                console.log(scope.width);
+                scope.height = $window.innerHeight;
+            });
+        });
+    };
+});
